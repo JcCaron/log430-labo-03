267a335d681e007e2aa2331eb3443a6d55247d93
Jean-Christophe Caron <Caronjc00@gmail.com>
2026-02-03 11:24:22 -0500

    added smoke test

--- src/tests/test_store_manager.py
+++ src/tests/test_store_manager.py
@@ -28,11 +28,44 @@ def test_stock_flow(client):
     
     assert response.status_code == 201
     data = response.get_json()
-    assert data['product_id'] > 0 
+    product_id = data['product_id']
+    assert product_id > 0 
 
     # 2. Ajoutez 5 unités au stock de cet article (`POST /stocks`)
+    quantity = 5
+    stock_payload = {'product_id': product_id, 'quantity': quantity}
+    response = client.post(
+        '/stocks',
+        data=json.dumps(stock_payload),
+        content_type='application/json'
+    )
+    assert response.status_code == 201
+
     # 3. Vérifiez le stock, votre article devra avoir 5 unités dans le stock (`GET /stocks/:id`)
+    response = client.get(f'/stocks/{product_id}')
+    stock_info = response.get_json()
+    assert stock_info['quantity'] == quantity
+
     # 4. Faites une commande de l'article que vous avez crée, 2 unités (`POST /orders`)
+    order_payload = {'user_id': 1, 'items': [{'product_id': product_id, 'quantity': 2}]}
+    response = client.post(
+        '/orders',
+        data=json.dumps(order_payload),
+        content_type='application/json'
+    )
+    assert response.status_code == 201
+    order = response.get_json()
+    order_id = order['order_id']
+
     # 5. Vérifiez le stock encore une fois (`GET /stocks/:id`)
-    # 6. Étape extra: supprimez la commande et vérifiez le stock de nouveau. Le stock devrait augmenter après la suppression de la commande.
-    assert "Le test n'est pas encore là" == 1
+    response = client.get(f'/stocks/{product_id}')
+    stock_info = response.get_json()
+    assert stock_info['quantity'] == 3
+
+     # 6. Étape extra: supprimez la commande et vérifiez le stock de nouveau. Le stock devrait augmenter après la suppression de la commande.
+    response = client.delete(f'/orders/{order_id}')
+    assert response.status_code == 200
+
+    response = client.get(f'/stocks/{product_id}')
+    stock_info = response.get_json()
+    assert stock_info['quantity'] == 5
